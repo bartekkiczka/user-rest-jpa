fefc4af55e40e6869c35d34ccc26c4f6bf38f33d
Bartoszek123 <44552510+Bartoszek123@users.noreply.github.com>
2020-04-27 19:24:21 +0200

    project update

--- src/main/java/com/example/demo/roleService/RoleService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.roleService;
-
-import com.example.demo.entity.Role;
-
-public interface RoleService {
-
-    public Role findByRole(String role);
-    public Role findById(Long id);
-}
